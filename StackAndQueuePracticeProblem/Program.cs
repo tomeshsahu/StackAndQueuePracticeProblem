b4fc9bb4eeaefec088ac1d1e67b04d83de551909
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-05-11 23:17:47 +0530

    [TOMEHSWAR]ADD:-PUSH ELMEENT ON STACK

---
+++ StackAndQueuePracticeProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StackAndQueuePracticeProblem;
+
+Console.WriteLine("Stack And Queue Practice Problem");
+bool check = true;
+while (check)
+{
+
+    Console.WriteLine("1. UC1-PushElementInStack");
+    Console.WriteLine("Enter option for Execute the Program");
+
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            UC1PushElementInStack stack = new UC1PushElementInStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            break;
+        
+            case 0:
+            check = false;
+            break;
+
+    }
+}
---
+++ StackAndQueuePracticeProblem/UC1PushElementInStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueuePracticeProblem
+{
+    public class UC1PushElementInStack
+    {
+        private Node top;
+        public UC1PushElementInStack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack" , value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
